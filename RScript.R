c71570b58c1d15d783a56557d7b45881ebe67bc1
Rick Coleman <do3bol85@yahoo.com>
2016-10-23 16:09:08 -0700

    Adding Rscript File

---
+++ RScript.R
@@ -0,0 +1,6 @@
+GDP <- read.csv ("CaseStudy1/GDP.csv",sep = ",",header = FALSE, skip=5,nrows = 190,col.names = c("Country","Ranking","Blank","Economy","MillionsOfUSDollars","V6","V7","V8","V9","V10"))
+GDP[6:10] <- NULL
+head(GDP)
+
+Educ <- read.csv("CaseStudy1/Educ.csv",sep = ",",header = TRUE)
+Head(Educ)
